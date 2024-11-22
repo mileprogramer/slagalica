d0f34893de8a53195e4a306c1fe0d56fdb4ab86c
mileprogramer <obecano.ispunjeno@gmail.com>
2024-11-22 21:19:18 +0100

    delete not needed files

--- Desktop/skocko/js/Results.js
+++
@@ -1,6 +0,0 @@
-const possibleResult = {
-    onPlace: true,
-    correctButNotGoodPlace: false,
-    notInCombination: null,
-};
-export default possibleResult;
--- Desktop/skocko/js/index.js
+++
@@ -1,7 +0,0 @@
-import Game from './Game.js';
-(function () {
-    let gameBody = document.querySelector(".game-body");
-    if (gameBody) {
-        const gameController = new Game(gameBody);
-    }
-})();
